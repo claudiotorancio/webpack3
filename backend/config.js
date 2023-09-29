88cb1ee8c23cd6b8a5a8993de416297a5944ca3f
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-29 01:42:39 -0300

    57 commit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
 const { config } = require("dotenv")
 config()
 }
@@ -7,7 +7,7 @@ config()
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 module.exports = {
     MONGODB_URI,
