d544b1ee3f76533f2541e62fe6764ef0f13b8bb5
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-28 23:25:21 -0300

    27 commit

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,5 @@
 
 
 
-const express = require('express')
-const morgan = require('morgan')
-const multer = require('multer')
-const path = require('path')
-const cors = require('cors');
 
 
-//initializacion
-const app = express()
-
-
-
-//Settings
-
-
-//Middlewares
-app.use(morgan('dev'));
-
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'backend/public/uploads'),
-    filename(req, file, cb) {
-        cb(null, new Date().getTime() + path.extname(file.originalname));
-    }
-});
-app.use(multer({storage}).single('image'));
-
-app.use(express.urlencoded({extended:false}));
-
-app.use(express.json());
-
-app.use(cors())
-
-
-app.use('/api/books', require('../api/books.js'))
-
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  });
-  
- 
-//Static files
-app.use(express.static(path.join(__dirname, 'backend', 'public')))
-
-module.exports = app;
