b4e7d6fe5c6b314ca52967afb052a429207afa59
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-30 15:07:46 -0300

    13 commit

--- backend/database.js
+++ backend/database.js
@@ -7,8 +7,10 @@ const { MONGODB_URI } = require("./config.js");
     try {
       const db = await connect(MONGODB_URI)
       console.log('DB is connected to', db.connection.name);
+    
     } catch (err) {
       console.error(err);
     }
+   
   })();
 
