c5dba01f88d7248748810e9825adc6609eab2fa8
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-30 13:21:01 -0300

    4 commit

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express()
 app.use(morgan('dev'));
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'backend/public/uploads'),
+    destination: path.join(__dirname, 'public/uploads'),
     filename(req, file, cb) {
         cb(null, new Date().getTime() + path.extname(file.originalname));
     }
@@ -43,6 +43,6 @@ app.use((err, req, res, next) => {
   
  
 //Static files
-app.use(express.static(path.join(__dirname, 'backend', 'public')))
+app.use(express.static(path.join(__dirname,  'public')))
 
 module.exports = app;
