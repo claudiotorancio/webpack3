1eae4f9cd8eb6f7cc4b6dc7d4d964c0edacc6def
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-01 04:12:31 -0300

    72 commit

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,33 @@
-//index.js
+// Importar las dependencias necesarias
+const app = require('./app');
+const { PORT } = require('./config');
+const connectDB = require('./database');
 
-const {PORT} = require('./config.js')
-const app = require('./app.js')
-require('./database.js')
+// Función para iniciar el servidor
+async function startServer() {
+  try {
+    // Conectar a la base de datos
+    await connectDB();
 
+    // Iniciar el servidor de Express
+    const server = app.listen(PORT, () => {
+      console.log(`Servidor en ejecución en el puerto ${PORT}`);
+    });
 
+    // Manejo de errores no controlados
+    process.on('unhandledRejection', (error) => {
+      console.error('Error no controlado:', error);
+      // Aquí puedes agregar lógica adicional de manejo de errores si es necesario
+    });
 
-//Server Starter
-app.listen(PORT, () => {
-    console.log('Server start on port', PORT);
-})
+    process.on('uncaughtException', (error) => {
+      console.error('Excepción no capturada:', error);
+      // Aquí puedes agregar lógica adicional de manejo de errores si es necesario
+    });
+  } catch (error) {
+    console.error('Error al iniciar el servidor:', error);
+  }
+}
 
-  
+// Llamamos a la función para iniciar el servidor
+startServer();
