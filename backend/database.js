7c38fc1d290b3d88a4f705bdedbe883ac3d27b27
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-01 18:05:58 -0300

    97 commit

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,21 @@
-
 const { connect } = require("mongoose");
-
 const { MONGODB_URI } = require("./config.js");
 
-  (async () => {
-    try {
-      const db = await connect(MONGODB_URI)
-      console.log('DB is connected to', db.connection.name);
-    
-    } catch (err) {
-      console.error(err);
-    }
-   
-  })();
+(async () => {
+  try {
+    const db = await connect(MONGODB_URI);
+    console.log('DB is connected to', db.connection.name);
+
+    // Realiza cualquier tarea que necesitas después de la conexión
+
+    // Luego, desconéctate y sale del proceso
+    await db.connection.close();
+    console.log('DB connection closed.');
+    process.exit(0); // 0 indica una salida exitosa
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // 1 indica una salida con error
+  }
+})();
+
 
