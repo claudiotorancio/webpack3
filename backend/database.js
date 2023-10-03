950c1333a78e9234518b9f7c42ff06c1338c5c4b
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-03 01:56:33 -0300

    156 commit

--- backend/database.js
+++ backend/database.js
@@ -1,23 +1,21 @@
-
-
-
-
-const { connect} = require("mongoose");
-
+const { connect } = require("mongoose");
 const { MONGODB_URI } = require("./config.js");
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-(async () => {
+async function startServer() {
   try {
-    const db = await connect(MONGODB_URI, options);
+    const db = await connect(MONGODB_URI);
     console.log('DB is connected to', db.connection.name);
+
+    // Tu lógica adicional aquí
+
+    // Finaliza el proceso de Node.js
+    process.exit(0);
   } catch (err) {
     console.error(err);
+    // Finaliza el proceso con un código de error si hay un problema
+    process.exit(1);
   }
+}
 
-})();
-
+// Iniciar la aplicación y la base de datos
+startServer();
