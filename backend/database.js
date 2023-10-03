9a9de7efedf1df694970f570f02609c76f21f800
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-03 18:51:57 -0300

    157 commit

--- backend/database.js
+++ backend/database.js
@@ -1,21 +1,46 @@
-const { connect } = require("mongoose");
+const { MongoClient } = require('mongodb');
 const { MONGODB_URI } = require("./config.js");
 
-async function startServer() {
+
+
+// URL de conexión a tu base de datos MongoDB
+const mongoURI = 'mongodb://localhost:27017/tu_basededatos';
+
+async function connectToDatabase() {
   try {
-    const db = await connect(MONGODB_URI);
-    console.log('DB is connected to', db.connection.name);
+    const client = new MongoClient(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    // Tu lógica adicional aquí
+    // Conectar a la base de datos de forma asincrónica
+    await client.connect();
 
-    // Finaliza el proceso de Node.js
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    // Finaliza el proceso con un código de error si hay un problema
-    process.exit(1);
+    console.log('Conexión a MongoDB exitosa');
+
+    // Realiza aquí las operaciones de MongoDB que necesites
+
+    // Finaliza la conexión y cierra el cliente
+    await client.close();
+    
+    console.log('Conexión cerrada');
+
+    return true; // Indica que la conexión fue exitosa
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+    return false; // Indica que hubo un error en la conexión
   }
 }
 
-// Iniciar la aplicación y la base de datos
-startServer();
+// Llama a la función de conexión
+connectToDatabase()
+  .then((success) => {
+    if (success) {
+      console.log('La conexión a MongoDB se realizó con éxito.');
+    } else {
+      console.log('La conexión a MongoDB falló.');
+    }
+  })
+  .catch((error) => {
+    console.error('Error en la promesa de conexión:', error);
+  });
--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,11 @@
 
 const {PORT} = require('./config.js')
 const app = require('./app.js')
-require('./database.js')
-
-
+const {startServer} = require('./database.js')
 
 //Server Starter
 app.listen(PORT);
         console.log('Server start on port', PORT);
 
-
+startServer
   
