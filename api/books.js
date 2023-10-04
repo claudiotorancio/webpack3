3e5b95fa69309c2724fe967e09b191a5cf63d5ef
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-04 09:09:40 -0300

    178 commit

--- api/books.js
+++ api/books.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 const {renderAllBooks, createBook, deleteBook} = require('./controllers.js')
 
-router.get('/', renderAllBooks);
+router.get('/api/books', renderAllBooks);
 
-router.post('/', createBook);
+router.post('/api/books', createBook);
 
-router.delete('/:id', deleteBook);
+router.delete('/api/books/:id', deleteBook);
 
 
 module.exports = router;
