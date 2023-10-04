66d37dab8497f494a7b09e6d9090dbb97cd89a99
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-04 09:54:31 -0300

    182 commit

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ module.exports = {
     entry: './frontend/app.js',
     output: {
         path: path.join(__dirname, 'backend/public'),
-        filename: 'js/bundle.js',
-        path: path.join(__dirname, 'backend/public/uploads'),
+        filename: 'js/bundle.js'
         },
         mode: 'production',
 
