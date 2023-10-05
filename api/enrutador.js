afd95a669cf9633e441342252fe03d09bd300c7c
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-05 17:04:38 -0300

    7 commit

--- api/enrutador.js
+++ api/enrutador.js
@@ -14,7 +14,7 @@ router.get('/api/renderAllbooks', renderAllbooks);
 
 router.post('/api/createBook', createBook);
 
-router.delete('/api/cutBook/:id', cutBook);
+router.delete('/api/cutBook/', cutBook);
 
 
 module.exports = router;
