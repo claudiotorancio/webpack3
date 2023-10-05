f236301230b4c7e396395bdff2b8a6ddda1b0a1b
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-10-05 17:36:02 -0300

    12 commit

--- api/cutBook.js
+++ api/cutBook.js
@@ -1,4 +1,4 @@
-
+// cutBook.js
 
 const mongoose = require('mongoose');
 const config = require('../config'); 
--- frontend/services/BookServices.js
+++ frontend/services/BookServices.js
@@ -30,16 +30,30 @@ class BookService {
     }
 
     async deleteBook(bookId) {
-        const URI = `${this.baseURI}/cutBook/${bookId}`;
-        const res = await fetch(URI, {
+        try {
+          const URI = `${this.baseURI}/cutBook/${bookId}`;
+          const res = await fetch(URI, {
             method: 'DELETE',
             headers: {
-                'Content-type': 'application/json'
+              'Content-type': 'application/json'
             }
-        });
-        const data = await res.json();
-        console.log(data);
-    }
+          });
+      
+          if (!res.ok) {
+            // Manejar errores de respuesta no exitosa aquí
+            console.error('Error en la solicitud DELETE:', res.statusText);
+            throw new Error('Failed to delete book');
+          }
+      
+          // Verificar si la respuesta es válida antes de analizarla como JSON
+          const data = await res.json();
+          console.log(data);
+        } catch (error) {
+          console.error(error);
+          // Manejar el error de manera adecuada aquí
+        }
+      }
+      
 }
 
 export default BookService;
